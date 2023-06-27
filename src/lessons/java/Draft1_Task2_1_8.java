9d0af51e4ed80a5dfac244cf19cdc632425f42df
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-28 01:00:40 +0300

    Initial commit

--- src/lessons/java/Draft1_Task2_1_8.java
+++ src/lessons/java/Draft1_Task2_1_8.java
@@ -17,17 +17,45 @@ public class Draft1_Task2_1_8 {
                 "\n\nРешение: ");
         int[] array = {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        System.out.println("Исходный массив: ");
         IntStream.of(array).forEach(a -> System.out.print(a + " "));
         System.out.println(); // Это перенос строки
 
         Random random = new Random();
-        IntStream s = random.ints(10, -10, 10);
-        s.filter((a) -> a > 0).forEach(a -> System.out.print(a + " "));
+        System.out.println("Создаём потоковый массив рандомных чисел в количестве 20 чисел, отфильтровываем " +
+                "\nиз него положительные числа, сортируем по возрастанию и выводим на печать:  ");
+        IntStream s = random.ints(20, -10, 10);
+        s.filter((a) -> a > 0).sorted().forEach(a -> System.out.print(a + " "));
         System.out.println(); // Это перенос строки
 
     }
 }
     //        Конец Примера _ КККККККККККККККК
+
+
+
+
+
+
+
+//    //        Пример 15 ППППППППППППППППППППППППППППППППППП Сделано кроме сортировки
+//    public static void main(String[] args) {
+//        System.out.println("Задание : \n1.\tПопробуйте создать поток рандомных чисел от -10  до 10, отфильтруйте их " +
+//                "\nтак, чтобы остались только положительные, отсортируйте по возрастанию и выведите их  в консоль" +
+//                "\n\nРешение: ");
+//        int[] array = {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//
+//        IntStream.of(array).forEach(a -> System.out.print(a + " "));
+//        System.out.println(); // Это перенос строки
+//
+//        Random random = new Random();
+//        IntStream s = random.ints(10, -10, 10);
+//        s.filter((a) -> a > 0).forEach(a -> System.out.print(a + " "));
+//        System.out.println(); // Это перенос строки
+//
+//    }
+//}
+//    //        Конец Примера 15 КККККККККККККККК
 
 
 
